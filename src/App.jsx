7497de82924b1d1dc8a84912780d8b55c3857e73
rahijamil <cs.mohammadrahi@gmail.com>
2025-10-23 21:18:47 +0600

    Implement 3D Product

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import ProductViewer from "./components/ProductViewer";
+
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <ProductViewer />
     </main>
   );
 };
